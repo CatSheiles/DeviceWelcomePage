3a9d72654756c1ba02eedae8ff5e2b8dd964576d
CatSheiles <cathysheiles@gmail.com>
2021-05-13 15:01:40 +1000

    completed js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // DOM elements
 const time = document.getElementById('time'),
     greeting = document.getElementById('greeting'),
-    name = document.getElementById('name'),
+    userName = document.getElementById('name'),
     focus = document.getElementById('focus');
+
+// Options
+const showAMPM = true;
 
 // Show time to update every second
 function showTime() {
@@ -18,7 +21,8 @@ function showTime() {
     hour = hour % 12 || 12;
 
     // output the time
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}
+        ${showAMPM ? amPm : ''}`;
 
     // call time in milliseconds
     setTimeout(showTime, 1000);
@@ -52,7 +56,69 @@ function setBackGreet() {
     }  
 }
 
+// get Name
+function getName() {
+   if(localStorage.getItem('name') === null) {
+    userName.textContent = '[Enter your Name]';
+   }else {
+    userName.textContent = localStorage.getItem('name');
+   }
+}
+
+// set Name
+function setName(e) {
+    if(e.type === 'keypress'){
+        // Make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            userName.blur(); //takes you out of state so extra lines dont appear
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+// get Focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null) {
+        focus.textContent = '[Add a Focus to Inspire you today!]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// set Focus
+function setFocus(e) {
+    if(e.type === 'keypress'){
+        // Make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur(); //takes you out of state so extra lines dont appear
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+
+userName.addEventListener('keypress', setName);
+userName.addEventListener('blur', setName); //blur is for when user leaves input field
+// clear username box on click if you wish to change to a different name
+userName.addEventListener('click', () => {
+    userName.innerHTML = '';
+})
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+// clean focus text on click if you wish to change text
+focus.addEventListener('click', () => {
+    focus.innerHTML = '';
+})
+
 // run
 showTime();
 setBackGreet();
+getName();
+getFocus();
+
+
 
